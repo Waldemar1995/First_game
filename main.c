3cc08229c2beeb86775e43fd042f1fb6ec971eac
Waldemar Kołodziejczyk <kolodziejczykwaldemar222@gmail.com>
2017-03-11 18:11:27 +0100

    Function for counting points V 0.0

--- main.c
+++ main.c
@@ -1,73 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
-#define kl 20   //zak³adana liczba klocków
-#define pl 40     //zak³adany rozmiaw planszy
 
-void init(int *id, int *p1, int *p2, int *p3, int *p4, int **pla){ //inicjalizacja klockow i planszy
+void printTable();
+ struct Element *table[3][3] = {{NULL}};
+ int i, s, k,l;
 
-    int i, m, n;
+ struct Element {
+    //char  title[6];
+    char  title;
+    //int pola[4];
+    char  top, right, bottom, left;
+    bool istop;
+    bool isright;
+    bool isbottom;
+    bool isleft; //true if avaliable, false if not
+    bool iscrossing;
+    bool istemple;
+    bool iscity;
+    int   rotation;
+    bool  available;
+    int   id;
 
-    for (i = 0; i<kl; i++){     //nadanie pocz¹tkowych wartoœci dla klocków
-        id[i] = i;
-        p1[i] = 0;
-        p2[i] = 0;
-        p3[i] = 0;
-        p4[i] = 0;
-    }
-    for (m = 0; m < pl; m++){     //nadanie pocz¹tkowych wartoœci dla planszy
-        for(n = 0; n < pl; n++) pla[m][n] = 0;
-    }
+ };
 
-}
+void printTable()
+ {
+     printf("Table: \n");
 
-void wylosuj(int *po1, int *po2, int *po3, int *po4){
-    int i;
-    srand ( time(NULL) );        //inicjalizaja generatora
-    for(i = 0; i<kl; i++){      // losowe przypisanie wartoœci 1, 2 lub 3
-        po1[i] = rand()%3+1;
-        po2[i] = rand()%3+1;
-        po3[i] = rand()%3+1;
-        po4[i] = rand()%3+1;
-    }
-}
+     for(k=0;k<3;k++)
+     {
+         for (l=0;l<3;l++)
+         {
+             (table[k][l] == NULL) ? (printf("%d\t",(k*(3))+l)) : (printf("%c\t", table[k][l]->title));
+         }
+        printf("\n");
+     }
+ printf("\n");
+ }
+
 
 int main()
 {
-    int i, j, k, l, m, n, p;
-
-    int plansza[pl][pl]; //tablica z identyfikatorami planszy do gry
-    int ID[kl]; //identyfikator ka¿dego z 20 kloców
-    int pole1[kl]; //pierwsze pole ka¿dego klocka, trzy opcje, 1-city, 2-road, 3-plain
-    int pole2[kl];
-    int pole3[kl];
-    int pole4[kl];
-
-// init (ID, pole1, pole2, pole3, pole4, plansza);
-    for (i = 0; i<kl; i++){     //nadanie pocz¹tkowych wartoœci dla klocków
-        ID[i] = i;
-        pole1[i] = 0;
-        pole2[i] = 0;
-        pole3[i] = 0;
-        pole4[i] = 0;
-    }
-
-    wylosuj(pole1, pole2, pole3, pole4);
-
-    for (m = 0; m<pl; m++){     //nadanie pocz¹tkowych wartoœci dla planszy
-        for(n = 0; n < pl; n++) plansza[m][n] = 0;
-    }
-
-    for (j = 0; j<kl; j++){     //testowe wyswietlanie
-        printf("%d \t %d \t %d \t %d \t %d\n", ID[j], pole1[j], pole2[j], pole3[j], pole4[j]);
-    }
 
 
-    for(k = 0; k<pl; k++){          //testowe wyswietlanie
-        for(l = 0; l<pl; l++) printf("%d\t", plansza[k][l]);
-        printf("\n");
-    }
-    printf("zaczynamy!\n");
-    return 0;
+ struct Element element[5];
+
+ char allElements [14][5] = {{ 'A', 'R', 'P', 'R', 'P' }, //Definicja MOZLIWYCH elementow na sztywno; nie ma np. combinacji C-C-R-R itd.
+                             { 'B', 'P', 'P', 'R', 'R' },
+                             { 'C', 'P', 'R', 'R', 'R' },
+                             { 'D', 'P', 'C', 'C', 'P' },
+                             { 'E', 'C', 'P', 'P', 'P' },
+                             { 'F', 'C', 'R', 'R', 'P' },
+                             { 'G', 'R', 'C', 'P', 'R' },
+                             { 'H', 'R', 'C', 'R', 'R' },
+                             { 'I', 'R', 'R', 'R', 'R' },
+                             { 'J', 'P', 'C', 'P', 'C' },
+                             { 'K', 'R', 'C', 'R', 'P' },
+                             { 'L', 'C', 'C', 'C', 'C' },  //emblem
+                             { 'M', 'P', 'P', 'P', 'R' },  //temple
+                             { 'N', 'P', 'P', 'P', 'P' }}; //temple
+
+    srand ( time(NULL) );
+
+     for(i=0;i<5;i++)
+     {
+      //   s = rand()%14;
+         element[i].top = allElements[i][1];
+         element[i].right = allElements[i][2];
+         element[i].bottom = allElements[i][3];
+         element[i].left = allElements[i][4];
+        element[i].istop = true;
+        element[i].isright = true;
+        element[i].isbottom = true;
+        element[i].isleft = true; //true if avaliable, false if not
+        element[i].iscrossing = false;
+        element[i].istemple = false;
+        element[i].iscity = false;
+         element[i].rotation = 0;
+         element[i].title = allElements[i][0];
+         element[i].id = i;
+         element[i].available = true;
+
+         if (s == 8 || s == 7 || s == 2) element[i].iscrossing = true;
+         if (s == 12 || s == 13) element[i].istemple = true;
+         if (s == 11) element[i].iscity = true;
+     }
+
+  // table[3][3] = { {NULL, &element[3], &element[1]},
+   //             {NULL, &element[4], &element[0]},
+   //                    {NULL, NULL, NULL}};
+        table[0][1] =  &element[1];
+        printf("%c", element[2].right);
+
+
+        printTable();
+        printf("%c", table[0][1]->left);
 }
+
